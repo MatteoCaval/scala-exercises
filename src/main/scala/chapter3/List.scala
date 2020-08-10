a70122e84a798042035b45735222b4628dd3c43e
Matteo Cavalluzzo <mcavalluzzo.dev@gmail.com>
2020-08-10 17:17:24 +0200

    improved dropWhile

--- src/main/scala/chapter3/List.scala
+++ src/main/scala/chapter3/List.scala
@@ -74,13 +74,8 @@ object List { // `List` companion object. Contains functions for creating and wo
 
   @scala.annotation.tailrec
   def dropWhile[A](l: List[A], f: A => Boolean): List[A] = l match {
-    case Nil => Nil
-    case Cons(h, t) =>
-      if (f(h)) {
-        dropWhile(t, f)
-      } else {
-        t
-      }
+    case Cons(h, t) if (f(h)) => dropWhile(t, f)
+    case _ => l
   }
 
   def init[A](l: List[A]): List[A] = ???
