dffe2074bac837aa864c0c6ff4d0616d22bc0ebf
Matteo Cavalluzzo <mcavalluzzo.dev@gmail.com>
2020-10-06 21:54:33 +0200

    combiner changes

--- src/main/scala/pps/lab06/Combiner.scala
+++ src/main/scala/pps/lab06/Combiner.scala
@@ -1,17 +1,17 @@
 package pps.lab06
 
 /** Implement trait Functions with an object FunctionsImpl such that the code
-  * in TryFunctions works correctly. To apply DRY principle at the best,
-  * note the three methods in Functions do something similar.
-  * Use the following approach:
-  * - find three implementations of Combiner that tell (for sum,concat and max) how
-  * to combine two elements, and what to return when the input list is empty
-  * - implement in FunctionsImpl a single method combiner that, other than
-  * the collection of A, takes a Combiner as input
-  * - implement the three methods by simply calling combiner
-  *
-  * When all works, note we completely avoided duplications..
-  */
+ * in TryFunctions works correctly. To apply DRY principle at the best,
+ * note the three methods in Functions do something similar.
+ * Use the following approach:
+ * - find three implementations of Combiner that tell (for sum,concat and max) how
+ * to combine two elements, and what to return when the input list is empty
+ * - implement in FunctionsImpl a single method combiner that, other than
+ * the collection of A, takes a Combiner as input
+ * - implement the three methods by simply calling combiner
+ *
+ * When all works, note we completely avoided duplications..
+ */
 
 trait Functions {
   def sum(a: List[Double]): Double
@@ -27,35 +27,39 @@ trait Combiner[A] {
   def combine(a: A, b: A): A
 }
 
-object MaxCombiner extends Combiner[Int] {
-  override def unit: Int = Int.MinValue
+object ImplicitCombiners {
 
-  override def combine(a: Int, b: Int): Int = if (a > b) a else b
+  implicit object maxCombiner extends Combiner[Int] {
+    override def unit: Int = Int.MinValue
+
+    override def combine(a: Int, b: Int): Int = if (a > b) a else b
+  }
+
+  implicit object stringCombiner extends Combiner[String] {
+    override def unit: String = ""
+
+    override def combine(a: String, b: String): String = a + b
+  }
+
+  implicit object sumCombiner extends Combiner[Double] {
+    override def unit: Double = 0.0
+
+    override def combine(a: Double, b: Double): Double = a + b
+  }
+
 }
-
-object ConcatCombiner extends Combiner[String] {
-  override def unit: String = ""
-
-  override def combine(a: String, b: String): String = a + b
-}
-
-object SumCombiner extends Combiner[Double] {
-  override def unit: Double = 0
-
-  override def combine(a: Double, b: Double): Double = a + b
-}
-
 
 object FunctionsImpl extends Functions {
 
-  override def sum(a: List[Double]): Double = combine(a, SumCombiner)
+  import ImplicitCombiners._
 
-  override def concat(a: Seq[String]): String = combine(a, ConcatCombiner)
+  override def sum(a: List[Double]): Double = combine(a)(sumCombiner)
 
-  override def max(a: List[Int]): Int = combine(a, MaxCombiner)
+  override def concat(a: Seq[String]): String = combine(a)(stringCombiner)
 
-  def combine[A](a: Seq[A], c: Combiner[A]): A = a.foldRight(c.unit)((res, item) => c.combine(res, item))
+  override def max(a: List[Int]): Int = combine(a)(maxCombiner)
 
+  def combine[A](a: Seq[A])(c: Combiner[A]): A = a.foldRight(c.unit)((res, item) => c.combine(res, item))
 
 }
 
